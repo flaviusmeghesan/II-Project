bc0ad394ad56ed9ac00a21c111b19a66852cbc6e
rlkk <flv.meghe@gmail.com>
2024-04-07 13:01:59 +0300

    created pages and added routes for them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
-export default function () {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-out" element={<SignOut />} />
+      <Route path="/about" element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+
+    </Routes>
+  </BrowserRouter>;
 }
