d6ca49cb5023574a8c14d814b369a773d901db66
rlkk <flv.meghe@gmail.com>
2024-04-07 14:08:06 +0300

    created a test api

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
     console.log("Connected to MongoDB!!!!");
-    })
+    })  
     .catch((err) => {
         console.log(err);
     });
-    
+
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!!!");
     }
 );
+
+app.use("/api/user",userRouter)
