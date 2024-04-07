93d4ca89f85d3bc29f4b0ac94fe5e114afa163c0
rlkk <flv.meghe@gmail.com>
2024-04-07 13:54:10 +0300

    created user model for db

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { type } from 'os';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
