9995417903eea172059f326ffe6b3cbbc9175865
rlkk <flv.meghe@gmail.com>
2024-04-07 13:23:24 +0300

    header styling and functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
 
+import React from "react"
+import { Link } from "react-router-dom"
 export default function Header() {
   return (
-    <header>Header</header>
+    <header className='bg-purple-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>   
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-purple-500'>Kings</span>
+            <span className='text-purple-700'>Rent</span>
+        </h1>
+        </Link>
+        <form>
+            <input type='text' placeholder='Search...' className='bg-purple-100 bg-transparent border-2 border-purple-200 rounded-lg p-3 focus:outline-none' />
+           
+        </form>
+        <ul className="flex gap-4">
+            <Link to='/'>
+            <li>Home</li>
+            </Link>
+            <Link to='/about'>
+            <li>About</li>
+            </Link>
+            <Link to='/sign-in'>
+            <li>Sign in</li>
+            </Link>
+        </ul>
+        </div>
+    </header>
   )
 }
