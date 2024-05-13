3bbafbb28cbe296d1606e00d7ec13761578b8bd9
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-13 22:45:12 +0300

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import Search from './pages/Search';
 
 export default function App() {
   return <BrowserRouter>
@@ -16,6 +17,7 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
+      <Route path='/search'element={<Search />} />
       <Route element={<PrivateRoute />} >
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing />} />
