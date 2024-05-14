4d26048e030d3d200a0fa5d900cde030b1ac1856
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-14 22:08:18 +0300

    added delete user button functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -139,6 +139,22 @@ export default function Profile() {
     setShowListingsError(true);
   }
   }
+  const handleListingDelete = async (listingId)=>{
+  try {
+    const res = await fetch('/api/listing/delete/' + listingId, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false){
+      console.log(data.message);
+      return;
+    }
+    setUserListings((prev) => prev.filter((listing) => listing._id !== listingId)); 
+    
+  } catch (error) {
+    console.log(error.message);
+  }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -220,7 +236,7 @@ export default function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col items-center">
-              <button className='text-red-900  rounded-lg'>Delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className='text-red-900  rounded-lg'>Delete</button>
               <button className='text-green-900 rounded-lg'>Edit</button>
             </div>
           </div>
