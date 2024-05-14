3c00c5548424994525baee74f9ee4503855fc0d4
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-14 22:34:58 +0300

    created update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -20,6 +21,22 @@ export const deleteListing = async (req, res, next) => {
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json("Listing deleted");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404,"Listing not found"));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,"Unauthorized"));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
