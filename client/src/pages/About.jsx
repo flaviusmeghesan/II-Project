d91dd8551bfa857731571615fb368a97a3379b12
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-16 13:32:05 +0300

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div>about</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800 text-center w-full'>About KingsRent</h1>
+      <br />
+      <p className='mb-4 text-slate-700'>Welcome to KingsRent, your trusted partner in real estate. 
+      Whether you're buying, selling, or renting, our platform is designed to make the 
+      process simple, efficient, and enjoyable. We understand that finding the perfect home or investment 
+      property is more than just a transaction – it's a journey, and we're here to guide you every step of the way.</p>
+      <h2 className='text-2xl font-bold mb-4 text-slate-800'>Our Mission</h2>
+      <p className='mb-4 text-slate-700'>At KingsRent, our mission is to connect people with their dream properties. We aim to 
+      provide a seamless and transparent real estate experience by leveraging cutting-edge technology, expert insights, and 
+      exceptional customer service. We believe that everyone deserves a place to call home, and we're committed to making that a 
+      reality for our clients. </p>
+      <h2 className='text-2xl font-bold mb-4 text-slate-800'>Why Choose Us</h2>
+      <p className='mb-4 text-slate-700'>At KingsRent, we prioritize trust and transparency, ensuring that our listings include 
+      detailed property information, high-quality photos, and virtual tours. Our innovative technology enhances your experience 
+      with user-friendly features and secure online transactions. With deep local expertise, our team offers valuable insights 
+      and personalized service. We are committed to excellence, striving to exceed your expectations and deliver outstanding 
+      results. Choose KingsRent for a reliable and exceptional real estate journey.</p>
+      <p className='text-slate-700 text-xl font-bold'>Thank you for choosing us!</p>
+    </div>
   )
 }
