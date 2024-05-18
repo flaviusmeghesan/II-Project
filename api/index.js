ab016d7ea968606b28a9a8f56e2aa1f1c19e194d
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-19 00:24:48 +0300

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -17,6 +17,8 @@ mongoose.connect(process.env.MONGO)
     });
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -32,6 +34,12 @@ app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 
 app.use("/api/listing",listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
