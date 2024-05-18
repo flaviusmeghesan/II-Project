33185580f32438794eca73c50adff55c70c1ac88
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-19 00:16:38 +0300

    completed homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -51,9 +51,8 @@ export default function Header() {
             <Link to='/profile'>
            { currentUser ? (
             <img className = 'rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
-           ) : (<li classname= 'text-slate-700 hover: underline'> Sign in</li> 
+           ) : (<li className= 'text-slate-700 hover: underline'> Sign in</li> 
            )}
-            {/* <li>Sign in</li> */}
             </Link>
         </ul>
         </div>
