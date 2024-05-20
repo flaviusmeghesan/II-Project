c18802130c8af596a5c726bcd6cb46a005083dca
flaviusmeghesan <flv.meghe@gmail.com>
2024-05-20 15:02:15 +0300

    update to use root .env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({ 
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+// Load .env file from the root directory
+config({ path: resolve(__dirname, '..', '.env') });
+console.log('VITE_FIREBASE_API_KEY:', process.env.VITE_FIREBASE_API_KEY);
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
+export default defineConfig({ 
+  server: {
+    proxy: {
+      '/api': {
         target: 'http://localhost:3000',
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
